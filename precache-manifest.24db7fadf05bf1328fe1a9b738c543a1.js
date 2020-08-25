363683ad0f5aaed0237936369c0efc511c58b50e
Duru-young Raymond <duruyoungcr@gmail.com>
2020-08-25 20:50:14 +0100

    First commit

---
+++ precache-manifest.24db7fadf05bf1328fe1a9b738c543a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7637634ec992c1e77b9d1d53eed7d8f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "382dba8fb231e7b93f89",
+    "url": "/static/css/main.9da2d1ac.chunk.css"
+  },
+  {
+    "revision": "0c8049033586da8f6a58",
+    "url": "/static/js/2.1542edc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1542edc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382dba8fb231e7b93f89",
+    "url": "/static/js/main.4674115b.chunk.js"
+  },
+  {
+    "revision": "bae492c776d6e417a041",
+    "url": "/static/js/runtime-main.1b461360.js"
+  }
+]);
